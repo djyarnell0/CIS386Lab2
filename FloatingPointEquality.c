c1c708a962496da828ff4bfd91fcd4559dd6eb08
djyarnell0 <cocsinglesdev@gmail.com>
2022-09-16 16:27:07 -0500

    Added Lab Files

---
+++ FloatingPointEquality.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ float a, x;
+ a=6.7;
+ if(a==6.7)
+  printf("1\n");
+ else
+  printf("2\n");
+
+
+ x=8.5;
+ if(x==8.5)
+  printf("1\n");
+ else
+  printf("2\n");
+
+}
